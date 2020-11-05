9905a9a5b554c1eac993fe179fa8c80be63524e3
Kawon <nowak480@gmail.com>
2020-11-05 19:04:40 +0100

    created register page view

---
+++ components/registerForm.vue
@@ -0,0 +1,47 @@
+<template>
+    <form class = 'loginRegisterForm'>
+        <div class = 'loginRegisterForm__contentWrapper'>
+            <textInput :defaultValue = "emailInputValue" />
+            <textInput :defaultValue = "passwordInputValue" :hidden = "true"/>
+            <textInput :defaultValue = "confirmPasswordInputValue" :hidden = "true"/>
+            <p class = 'loginRegisterForm__contentWrapper__paragraph'>
+                Do you have an account?<NuxtLink to = "/login"> Log In! </NuxtLink> 
+            </p>
+        </div>
+        <submitBtn btnText = 'Sign Up' />
+    </form>
+</template>
+
+<script>
+import textInput from './textInput';
+import submitBtn from './submitBtn';
+
+export default {
+    components: {
+        textInput,
+        submitBtn,
+    },
+
+    props:{
+        emailInputValue: {
+            type: String,
+            required: true,
+        },
+        passwordInputValue: {
+            type: String,
+            required: true,
+        },
+        confirmPasswordInputValue: {
+            type: String,
+            required: true,
+        }
+    },
+
+    }
+</script>
+<style scoped lang = 'scss'>
+@import '../assets/globalScss/globals.scss';
+@import '../assets/globalScss/animations.scss';
+@import './styles/loginRegisterFormStyles.scss';
+
+</style>
--- pages/register.vue
+++ pages/register.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="container">
+  <div class="page loginRegisterPage">
+      <registerForm
+            :emailInputValue = "emailInputValue" 
+            :passwordInputValue = "passwordInputValue"
+            :confirmPasswordInputValue = "confirmPasswordInputValue"
+        />
   </div>
 </template>
 
 <script>
-export default {
+import registerForm from '../components/registerForm';
 
+export default {
+    components: {
+        registerForm,
+    },
+    data: function(){
+        return({
+            emailInputValue: 'Email',
+            passwordInputValue: 'Password',
+            confirmPasswordInputValue: 'Confirm password',
+        })
+    }
 }
 </script>
 
 <style>
-
+    @import './styles/loginRegisterPageStyles.scss';
 </style>
