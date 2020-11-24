e09d985f87c57217c19f70daa5dc0ea0fcbc38a0
Jan Kamienicki <nowak480@gmail.com>
2020-11-24 13:45:03 +0100

    Created layout for pages that require logged user

---
+++ layouts/logged.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <navbar />
+    <hamburgerBtn />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import navbar from "../components/navbar";
+import hamburgerBtn from "../components/hamburgerBtn";
+
+export default {
+  components: {
+    navbar,
+    hamburgerBtn
+  }
+};
+</script>
